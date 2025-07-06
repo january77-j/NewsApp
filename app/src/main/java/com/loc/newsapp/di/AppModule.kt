6a4cc06baa25caf3bca8312db00d60879b07d864
january77-j <xujiaqi0107@gmail.com>
2025-07-06 12:17:18 +0800

    create app module

---
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.loc.newsapp.di
+
+import android.app.Application
+import com.loc.newsapp.data.manager.LocalUserManagerImpl
+import com.loc.newsapp.domain.manager.LocalUserManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideLocalUserManager(
+        application: Application
+    ): LocalUserManager = LocalUserManagerImpl(application)
+}
